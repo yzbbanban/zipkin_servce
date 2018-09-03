17332792c7286cbe4fccf818ee768ca62a653916
yzbbanban <yzbbanban@live.com>
2018-09-03 23:30:16 +0800

    添加 rabbitmq

--- src/main/java/com/yzbbanban/zipkin_servce/ZipkinServceApplication.java
+++ src/main/java/com/yzbbanban/zipkin_servce/ZipkinServceApplication.java
@@ -3,10 +3,12 @@ package com.yzbbanban.zipkin_servce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableZipkinStreamServer
 @EnableAutoConfiguration
 public class ZipkinServceApplication {
 
